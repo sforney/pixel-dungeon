6a4a2301e02566e45b95c391a4e13a9fc73a904e
Shawn Forney <shawn.forney@neustar.biz>
2015-03-02 09:15:12 -0500

    Added extra potion test

--- test/com/forney/pixeldungeon/items/potions/PotionTest.java
+++ test/com/forney/pixeldungeon/items/potions/PotionTest.java
@@ -1,5 +1,8 @@
 package com.forney.pixeldungeon.items.potions;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -19,16 +22,23 @@ import com.watabou.pixeldungeon.items.potions.PotionOfMindVision;
 import com.watabou.pixeldungeon.items.potions.PotionOfParalyticGas;
 import com.watabou.pixeldungeon.items.potions.PotionOfStrength;
 import com.watabou.pixeldungeon.items.potions.PotionOfToxicGas;
+import com.watabou.pixeldungeon.items.potions.PotionType;
 import com.watabou.pixeldungeon.utils.StringResolver;
 
 @RunWith(JUnit4.class)
 public class PotionTest {
+	private StringResolver resolver;
+	private PotionInfo potionInfo;
+	
+	@Before()
+	public void setUp() {
+		resolver = new TestStringResolver();
+		potionInfo = new PotionInfo(resolver, new TestRandom(2));
+		potionInfo.randomize();		
+	}
+	
 	@Test
 	public void testCreate() {
-		StringResolver resolver = new TestStringResolver();
-		PotionInfo potionInfo = new PotionInfo(resolver, new TestRandom(2));
-		potionInfo.randomize();
-		
 		@SuppressWarnings("unused")
 		Potion pot = new PotionOfHealing(potionInfo, resolver);
 		pot = new PotionOfExperience(potionInfo, resolver);
@@ -41,6 +51,15 @@ public class PotionTest {
 		pot = new PotionOfMindVision(potionInfo, resolver);
 		pot = new PotionOfParalyticGas(potionInfo, resolver);
 		pot = new PotionOfToxicGas(potionInfo, resolver);
+	}
+	
+	@Test
+	public void testIdentify() {
+		Potion pot = new PotionOfHealing(potionInfo, resolver);
+		assertFalse(potionInfo.isKnown(PotionType.Healing));
+		assertFalse(pot.isIdentified());
+		pot.identify();
+		assertTrue(pot.isIdentified());
 	}
 	
 }
